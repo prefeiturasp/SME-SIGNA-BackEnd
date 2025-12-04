4bb599b17dafeb23ba56c020f62951a38763f5be
vferreira90 <vef_90kl@hotmail.com>
2025-12-04 14:29:38 -0300

    correcoes sonar

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,7 +1,11 @@
-# config/settings/test.py
-from . import local  # ou from . import base
+from . import base as base_settings  # ou local as base_settings, se preferir
 
-# Use SQLite em memória para testes
+# Copia todas as variáveis em MAIÚSCULAS do base_settings para este módulo
+for name in dir(base_settings):
+    if name.isupper():
+        globals()[name] = getattr(base_settings, name)
+
+# Agora sobrescreve apenas o que você quer para testes:
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
