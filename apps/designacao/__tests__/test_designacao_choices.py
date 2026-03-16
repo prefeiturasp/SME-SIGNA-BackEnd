aebde8833e2581cd9202a95ae769515fb4ab7d38
Marcelo Alves <alvesmarcelinho7@gmail.com>
2026-03-16 14:58:35 -0300

    test: :white_check_mark: Corrige testes

--- apps/designacao/__tests__/test_designacao_choices.py
+++ apps/designacao/__tests__/test_designacao_choices.py
@@ -1,30 +1,30 @@
-import pytest
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
-from unittest.mock import patch
-from django.contrib.auth import get_user_model
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory, force_authenticate
+from unittest.mock import patch, MagicMock
+from apps.designacao.api.views.designacao_choices_view import ImpedimentoSubstituicaoChoicesView
+from apps.designacao.models import Designacao
 
-class TestImpedimentoSubstituicaoChoicesView(APITestCase):
+class TestImpedimentoSubstituicaoChoicesView(TestCase):
 
     @patch('apps.designacao.models.Designacao.ImpedimentoSubstituicao')
     def test_get_impedimento_substituicao_choices_success(self, mock_choices_enum):
-        User = get_user_model()
-        user = User.objects.create_user(username='testuser')
-        self.client.force_authenticate(user=user)
-
         mock_choices_enum.choices = [
             ('valor_1', 'Label 1'),
             ('valor_2', 'Label 2'),
         ]
 
-        url = '/api/designacao/designacoes/impedimentos/'
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        factory = APIRequestFactory()
+        request = factory.get('/qualquer-url/')
         
+        user = MagicMock()
+        force_authenticate(request, user=user)
+
+        view = ImpedimentoSubstituicaoChoicesView.as_view()
+        response = view(request)
+
+        self.assertEqual(response.status_code, 200)
         expected_data = [
             {"value": "valor_1", "label": "Label 1"},
             {"value": "valor_2", "label": "Label 2"}
         ]
-        self.assertEqual(response.json(), expected_data)
+        self.assertEqual(response.data, expected_data)
